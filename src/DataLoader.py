f2248cddb4a8b183318176445098f5d1f9438f99
Vipooshan Vipulananthan <77396714+Vipooshan-1998@users.noreply.github.com>
2025-11-26 19:23:39 +0530

    Update DataLoader.py

--- src/DataLoader.py
+++ src/DataLoader.py
@@ -12,62 +12,134 @@ import networkx
 import itertools
 
 
+# class DADDataset(Dataset):
+#     def __init__(self, data_path, feature, phase='training', toTensor=False, device=torch.device('cuda'), vis=False):
+#         self.data_path = os.path.join(data_path, feature + '_features')
+#         self.feature = feature
+#         self.phase = phase
+#         self.toTensor = toTensor
+#         self.device = device
+#         self.vis = vis
+#         self.n_frames = 100
+#         self.n_obj = 19
+#         self.fps = 20.0
+#         self.dim_feature = self.get_feature_dim(feature)
+
+#         filepath = os.path.join(self.data_path, phase)
+#         self.files_list = self.get_filelist(filepath)
+
+#     def __len__(self):
+#         return len(self.files_list)
+
+#     def get_feature_dim(self, feature_name):
+#         if feature_name == 'vgg16':
+#             return 4096
+#         elif feature_name == 'res101':
+#             return 2048
+#         else:
+#             raise ValueError
+
+#     def get_filelist(self, filepath):
+#         assert os.path.exists(filepath), "Directory does not exist: %s" % (filepath)
+#         file_list = []
+#         for filename in sorted(os.listdir(filepath)):
+#             file_list.append(filename)
+#         return file_list
+
+#     def __getitem__(self, index):
+#         data_file = os.path.join(self.data_path, self.phase, self.files_list[index])
+#         assert os.path.exists(data_file)
+#         try:
+#             data = np.load(data_file)
+#             features = data['data']
+#             labels = data['labels']
+#             detections = data['det']
+
+#             if features.shape[0] == 10:
+#                 features = features[0]
+#             if labels.shape[0] == 10:
+#                 labels = labels[0]
+#             if detections.shape[0] == 10:
+#                 detections = detections[0]
+                
+#         except:
+#             raise IOError('Load data error! File: %s' % (data_file))
+#         if labels[1] > 0:
+#             toa = [90.0]
+#         else:
+#             toa = [self.n_frames + 1]
+
+#         if self.toTensor:
+#             features = torch.Tensor(features).to(self.device)
+#             labels = torch.Tensor(labels).to(self.device)
+#             toa = torch.Tensor(toa).to(self.device)
+
+#         if self.vis:
+#             video_id = str(data['ID'])[5:11]
+#             return features, labels, toa, detections, video_id
+#         else:
+#             return features, labels, toa
+
+
 class DADDataset(Dataset):
-    def __init__(self, data_path, feature, phase='training', toTensor=False, device=torch.device('cuda'), vis=False):
-        self.data_path = os.path.join(data_path, feature + '_features')
-        self.feature = feature
+    def __init__(self, data_path, phase='training', toTensor=False, device=torch.device('cuda'), vis=False, n_frames=100, fps=20, toa=None):
+        self.data_path = os.path.join(data_path, "obj_feat")
+        self.toa_dir = os.path.join(data_path, "toas")
         self.phase = phase
         self.toTensor = toTensor
+        self.toa = toa
         self.device = device
         self.vis = vis
-        self.n_frames = 100
+        self.n_frames = n_frames
         self.n_obj = 19
-        self.fps = 20.0
-        self.dim_feature = self.get_feature_dim(feature)
+        self.fps = fps
+        self.dim_feature = 4096
 
         filepath = os.path.join(self.data_path, phase)
         self.files_list = self.get_filelist(filepath)
 
     def __len__(self):
-        return len(self.files_list)
+        data_len = len(self.files_list)
+        return data_len
 
-    def get_feature_dim(self, feature_name):
-        if feature_name == 'vgg16':
-            return 4096
-        elif feature_name == 'res101':
-            return 2048
-        else:
-            raise ValueError
+    # def get_feature_dim(self, feature_name):
+    #     if feature_name == 'vgg16':
+    #         return 4096
+    #     elif feature_name == 'res101':
+    #         return 2048
+    #     else:
+    #         raise ValueError
 
     def get_filelist(self, filepath):
-        assert os.path.exists(filepath), "Directory does not exist: %s" % (filepath)
+        assert os.path.exists(filepath), "Directory does not exist: %s"%(filepath)
         file_list = []
         for filename in sorted(os.listdir(filepath)):
             file_list.append(filename)
         return file_list
+    
+    def get_toa(self, filename):
+        with open(os.path.join(self.toa_dir, filename[:-4] + ".txt")) as file:
+            toa = int(file.read())
+        return toa
 
     def __getitem__(self, index):
         data_file = os.path.join(self.data_path, self.phase, self.files_list[index])
         assert os.path.exists(data_file)
         try:
             data = np.load(data_file)
-            features = data['data']
-            labels = data['labels']
-            detections = data['det']
-
-            if features.shape[0] == 10:
-                features = features[0]
-            if labels.shape[0] == 10:
-                labels = labels[0]
-            if detections.shape[0] == 10:
-                detections = detections[0]
-                
+            features = data['data']  # n_frames x 20 x 4096
+            labels = data['labels']  # 2
+            detections = data['det']  # n_frames x 19 x 6
         except:
-            raise IOError('Load data error! File: %s' % (data_file))
+            raise IOError('Load data error! File: %s'%(data_file))
         if labels[1] > 0:
-            toa = [90.0]
+            if self.toa != None:
+                toa = [self.toa]
+            else:
+                toa = [self.get_toa(self.files_list[index])]
         else:
             toa = [self.n_frames + 1]
+        
 
         if self.toTensor:
             features = torch.Tensor(features).to(self.device)
@@ -191,3 +263,4 @@ class CrashDataset(Dataset):
             return features, labels, toa, detections, vid, video_path
         else:
             return features, labels, toa, video_path
+
